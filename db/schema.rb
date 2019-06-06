525e0d07451b8c1f0935ba6dacea28c8b5245903
waxime <aveline.maxime@gmail.com>
2019-06-06 17:39:44 +0200

    Add Category entity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_135357) do
+ActiveRecord::Schema.define(version: 2019_06_06_152550) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
