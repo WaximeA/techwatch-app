4d4792f93d4668b18b1c51d345402e9e18af4b9d
waxime <aveline.maxime@gmail.com>
2019-07-18 18:34:21 +0200

    Update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -45,12 +45,15 @@ ActiveRecord::Schema.define(version: 2019_07_16_151929) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "firstname"
+    t.string "lastname"
+    t.string "pseudo"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["id"], name: "index_users_on_id", unique: true
   end
 
 end
