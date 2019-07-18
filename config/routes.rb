b0d408953bac7f11cb29e71b52bf2c8c12b3b48b
waxime <aveline.maxime@gmail.com>
2019-07-18 18:39:43 +0200

    Add custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
+  resources :users
   resources :categories
   resources :comments
   resources :posts
 
+  # Custom User routes
+  get 'users/:id/categories', to: 'users#get_categories'
+  get 'users/:id/posts', to: 'users#get_posts'
+  get 'users/:id/comments', to: 'users#get_comments'
+
+  # Custom Post routes
   get 'posts/:id/comments', to: 'posts#get_comments'
+
+  # Custom Category routes
   get 'categories/:id/posts', to: 'categories#get_posts'
 
-  api_guard_routes for: 'users'
+  api_guard_routes for: 'users', controller: {
+      registration: 'users/registration'
+  }
 end
